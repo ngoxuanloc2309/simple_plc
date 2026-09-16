b0f1b6af7bf13599c9f9c0bec30be5427cf07f25
ngoxuanloc2309 <ngoxuanloc23092004@gmail.com>
2026-09-16 17:36:09 +0700

    add cmake

--- simple_plc/port/modbus_serial/modbus_serial.h
+++ simple_plc/port/modbus_serial/modbus_serial.h
@@ -2,5 +2,7 @@
 #define MODBUS_SERIAL_H
 
 #include "stdint.h"
+#include "stdbool.h"
+#include "nanomodbus.h"
 
 #endif    // MODBUS_SERIAL_H
---
+++ simple_plc/port/modbus_tcp/modbus_tcp.h
@@ -0,0 +1,8 @@
+#ifndef MODBUS_TCP_H
+#define MODBUS_TCP_H
+
+#include "stdint.h"
+#include "stdbool.h"
+#include "nanomodbus.h"
+
+#endif    // MODBUS_TCP_H
